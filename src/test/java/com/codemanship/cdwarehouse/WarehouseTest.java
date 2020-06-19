e83eb3db25afafaad6e1a1667c3881b76e78c54f
Jason Gorman <jason.gorman@codemanship.com>
2020-06-19 11:45:55 +0100

    Blah

--- src/test/java/com/codemanship/cdwarehouse/WarehouseTest.java
+++ src/test/java/com/codemanship/cdwarehouse/WarehouseTest.java
@@ -1,13 +1,16 @@
 package com.codemanship.cdwarehouse;
-
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class WarehouseTest {
 
     @Test
-    public void dummyTest(){
-        assertTrue(true);
+    public void findsCdInCatalogue(){
+        CD cd = new CD("Gary Numan", "The Pleasure Principle");
+        Warehouse warehouse = new Warehouse(List.of(cd));
+        assertEquals(cd, warehouse.search("Gary Numan", "The Pleasure Principle"));
     }
 }
