1281874a5bc0c95ffce5781a62b3a0d95328490d
Jason Gorman <jason.gorman@codemanship.com>
2020-06-19 10:11:39 +0100

    Second commit

---
+++ src/test/com/codemanship/cdwarehouse/WarehouseTest.java
@@ -0,0 +1,13 @@
+package com.codemanship.cdwarehouse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class WarehouseTest {
+
+    @Test
+    public void dummyTest(){
+        assertTrue(true);
+    }
+}
